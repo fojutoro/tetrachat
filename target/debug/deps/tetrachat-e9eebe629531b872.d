b98a26696c9a7751a755805b7fe45d8466049ef1
Michal Adam <78566636+Michal-Adam@users.noreply.github.com>
2023-01-20 16:30:27 +0100

    Delete target directory

--- target/debug/deps/tetrachat-e9eebe629531b872.d
+++
@@ -1,5 +0,0 @@
-/Users/fojutoro/code/soc/tetrachat/target/debug/deps/tetrachat-e9eebe629531b872.rmeta: src/main.rs
-
-/Users/fojutoro/code/soc/tetrachat/target/debug/deps/tetrachat-e9eebe629531b872.d: src/main.rs
-
-src/main.rs:
